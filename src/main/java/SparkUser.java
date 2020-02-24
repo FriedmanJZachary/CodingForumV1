62ec59fcf52788324b236bbf58e7a8ecdffabb49
Zachary Friedman <friedmanjzachary.com>
2020-02-24 15:49:13 -0500

    Added user creation

--- src/main/java/SparkUser.java
+++ src/main/java/SparkUser.java
@@ -3,8 +3,15 @@ import static spark.Spark.*;
 public class SparkUser {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World \n");
+
+        get("/hello/:name/:password", (req,res)->{
+            String pass = req.params(":password");
+            System.out.println(pass);
+            return "Hello, "+ req.params(":name") + "\n";
+        });
     }
 
     // I am testing a git commit
     // adding a comment
 }
+
