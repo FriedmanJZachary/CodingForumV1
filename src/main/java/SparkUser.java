d9ed14b233a8e5376b3659e472ef192ef9e59d8d
Theo Jaquenoud <theojaquenoud@Theos-MacBook-Pro-1336.local>
2020-02-24 15:22:14 -0500

    Added a new line

--- src/main/java/SparkUser.java
+++ src/main/java/SparkUser.java
@@ -2,7 +2,7 @@ import static spark.Spark.*;
 
 public class SparkUser {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World /n");
+        get("/hello", (req, res) -> "Hello World \n");
     }
 
     // I am testing a git commit
