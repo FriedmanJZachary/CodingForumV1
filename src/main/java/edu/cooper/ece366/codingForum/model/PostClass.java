a3a7cc4e33730ea9fb4afa9fff3a3dd20f2a74af
theojaquenoud <thjaquenoud@gmail.com>
2020-03-02 15:05:33 -0500

    Added proper ID using Atomic variables

--- src/main/java/edu/cooper/ece366/codingForum/model/PostClass.java
+++ src/main/java/edu/cooper/ece366/codingForum/model/PostClass.java
@@ -3,28 +3,29 @@ package edu.cooper.ece366.codingForum.model;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class PostClass {
     // Instance Variables
-    private final Long id;
     private final Integer type; //coding problem or post/question
     private final String author; //UserClass.id
 
     private String post;    //body of text containing the post
     private String tags[];  //each string is a tag
 
+    static final AtomicLong NEXT_ID = new AtomicLong(0);
+    final long id = NEXT_ID.getAndIncrement();
     private final Timestamp timestamp;
     private Map<Integer, String> likes = new HashMap<Integer, String>();
 
-    public PostClass(Long id, Integer type, String author, String post, String tags[]){
-        this.id = id;
+    public PostClass(Integer type, String author, String post, String tags[]){
         this.type = type;
         this.author = author;
         this.post = post;
         this.tags = tags;
 
         timestamp = new Timestamp(System.currentTimeMillis());
-     //   likes = new Map<Integer, String>();
+        //   likes = new Map<Integer, String>();
 
     }
 
--- src/main/java/edu/cooper/ece366/codingForum/store/PostStoreImpl.java
+++ src/main/java/edu/cooper/ece366/codingForum/store/PostStoreImpl.java
@@ -11,19 +11,28 @@ import java.util.Map;
 
 public class PostStoreImpl implements PostStore{
 
-    private HashMap<UserClass, List<PostClass>> userpostMap;
+    private Map<UserClass, List<PostClass>> userpostMap;
     private List<PostClass> postList;
 
-    public PostStoreImpl() { this.postList = new ArrayList<>(); }
-    public PostStoreImpl(List<PostClass> postList) {
-        this.postList = postList;
+//    public PostStoreImpl() { this.postList = new ArrayList<>(); }
+//    public PostStoreImpl(List<PostClass> postList) {
+//        this.postList = postList;
+//    }
+
+    public PostStoreImpl(){ this.userpostMap = new HashMap<>();}
+    public PostStoreImpl(Map<UserClass, List<PostClass>> userpostMap){ this.userpostMap = userpostMap; }
+
+    public PostStoreImpl(PostClass newPost){
+//        userpostMap.put( newPost.getId(), newPost);
+
     }
+
+//Map<String, List<User>> usersByCountry = listOfUsers.stream().collect(Collectors.groupingBy(User::getCountry));
 
 //    public PostClass getPost(PostClass post){
 //        return new PostClass();
 //    }
 // We need to figure out where we are getting these posts from
-
 
     public void addLike(PostClass post, UserClass user){
         throw new UnsupportedOperationException("Error: addLike not implemented yet\n");
