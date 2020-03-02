6f400553d9aca9e402f1a7ebd75176e4005ea8f0
theojaquenoud <thjaquenoud@gmail.com>
2020-03-02 15:52:49 -0500

    Made working PostStoreImpl

--- src/main/java/edu/cooper/ece366/codingForum/store/PostStoreImpl.java
+++ src/main/java/edu/cooper/ece366/codingForum/store/PostStoreImpl.java
@@ -4,26 +4,38 @@ import edu.cooper.ece366.codingForum.model.PostClass;
 import edu.cooper.ece366.codingForum.model.UserClass;
 import spark.ResponseTransformer;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class PostStoreImpl implements PostStore{
+public class PostStoreImpl implements PostStore {
 
-    //private Map<Long id, PostClass> postList = new HashMap<Long, PostClass>;
+    private Map<Long, PostClass> postList;
 
+    public PostStoreImpl() {
+        this.postList = new HashMap<>();
+    }
+    public PostStoreImpl(Map<Long, PostClass> postList) {this.postList = postList;}
+//    public PostStoreImpl(PostClass post) {
+//        postList.
+//    }
 
+    @Override
+    public PostClass getPost(Long id) {
+        PostClass post = postList.get(id);
+        return post;
+    }
 
-
+    @Override
     public void addLike(PostClass post, UserClass user){
         throw new UnsupportedOperationException("Error: addLike not implemented yet\n");
     }
 
+    @Override
     public void addTag(PostClass post, String tag){
         throw new UnsupportedOperationException("Error: addTag not implemented yet\n");
     }
 
+    @Override
     public void editPost(PostClass post, String text){
         throw new UnsupportedOperationException("Error: editPost not implemented yet\n");
     }
