7df5a266d4d21f1e03ee99b1c41f617a87fc01b4
Zachary Friedman <friedmanjzachary.com>
2020-03-04 11:50:20 -0500

    Cleanup 2

--- src/main/java/edu/cooper/ece366/codingForum/model/UserClass.java
+++ src/main/java/edu/cooper/ece366/codingForum/model/UserClass.java
@@ -16,14 +16,9 @@ public class UserClass {
         this.pass = pass;
     }
 
-    //Name display method
     public String getName() { return userName; }
-
     public void addPost(Long post) { postList.add(post); }
-
     public void removePost(Long post) { postList.remove(post); }
-    
     public ArrayList<Long> returnPost(Long post) { return postList; }
-
 
 }
